9461a5f7c683e4c8151a23cb4e283e01e15c6eb1
김지윤 <b01090443093@gmail.com>
2026-07-08 14:30:20 +0900

    [Fix] 에러 코드 포멧 변경

--- src/main/java/mutsa_vacation_week1/demo/global/exception/ErrorCode.java
+++ src/main/java/mutsa_vacation_week1/demo/global/exception/ErrorCode.java
@@ -4,16 +4,22 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+// 에러 정의서 
 public enum ErrorCode {
-    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "메뉴를 찾을 수 없습니다."),
-    MENU_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "옵션을 찾을 수 없습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+    // MENU 도메인
+    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "MENU404_1", "메뉴를 찾을 수 없습니다."),
+    MENU_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "MENU404_2", "옵션을 찾을 수 없습니다."),
+
+    // COMMON 도메인
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500_1", "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
+    private final String name;
     private final String message;
 
-    ErrorCode(HttpStatus httpStatus, String message) {
+    ErrorCode(HttpStatus httpStatus, String name, String message) {
         this.httpStatus = httpStatus;
+        this.name = name;
         this.message = message;
     }
 }
--- src/main/java/mutsa_vacation_week1/demo/global/exception/ErrorResponse.java
+++ src/main/java/mutsa_vacation_week1/demo/global/exception/ErrorResponse.java
@@ -1,10 +1,11 @@
 package mutsa_vacation_week1.demo.global.exception;
 
+// 에러 반환 포맷
 public record ErrorResponse(
         String code,
         String message
 ) {
     public static ErrorResponse from(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+        return new ErrorResponse(errorCode.getName(), errorCode.getMessage());
     }
 }
