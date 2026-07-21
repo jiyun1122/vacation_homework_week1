d9f2784735a5a0334121c59ebf135db5352b3c38
yun <coolpyh77@gmail.com>
2026-07-21 15:56:10 +0900

    [fix] 500 error 해결

--- src/main/java/mutsa_vacation_week1/demo/cart/dto/request/CartItemUpdateRequest.java
+++ src/main/java/mutsa_vacation_week1/demo/cart/dto/request/CartItemUpdateRequest.java
@@ -2,10 +2,12 @@ package mutsa_vacation_week1.demo.cart.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class CartItemUpdateRequest {
     private int quantity;
